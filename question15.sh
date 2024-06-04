13b2efa8b079fdc06573997ab9ce1f3d93d48b3c
Elwin12121 <1208279221@qq.com>
2024-06-04 14:30:15 +0930

    q15

---
+++ question15.sh
@@ -0,0 +1,8 @@
+git clone  https://github.com/ian-knight-uofa/git-practice-01.git
+cd git-practice-01
+git fetch origin
+git checkout branch1
+git checkout main
+git merge branch1
+git add .
+git commit -m"q15"
